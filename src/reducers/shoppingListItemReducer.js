c095145f2f4af5c7e08d7273ed2af79f4ef95883
yuliyambg <yuliyambg@gmail.com>
2021-05-06 21:56:04 -0400

    complete

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -4,13 +4,18 @@ export default function shoppingListItemReducer(
 	},
 	action
 ) {
+	console.log(action);
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log('Current state.items length %s', state.items.length);
+			console.log('Updating state.items length to %s', state.items.length + 1);
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
-			}
+			};
+
 		default:
+			console.log('Initial state.items length: %s', state.items.length);
 			return state;
 	}
 }
